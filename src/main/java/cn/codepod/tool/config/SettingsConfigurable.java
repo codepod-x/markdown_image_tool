0f25a25d9067ce77953f7f9153b3e2e2bf8db3d3
yangli <517498696@qq.com>
2022-04-02 17:35:14 +0800

    添加设置

---
+++ src/main/java/cn/codepod/tool/config/SettingsConfigurable.java
@@ -0,0 +1,42 @@
+package cn.codepod.tool.config;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class PluginSettingsConfigurable implements SearchableConfigurable {
+
+    public PluginSettingsConfigurable() {
+    }
+
+    @Override
+    public @NotNull @NonNls String getId() {
+        return null;
+    }
+
+    @Override
+    public @NlsContexts.ConfigurableName String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public @Nullable JComponent createComponent() {
+        return null;
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
+}
---
+++ src/main/java/cn/codepod/tool/entity/SettingConfig.java
@@ -0,0 +1,2 @@
+package cn.codepod.tool.entity;public class SettingConfig {
+}
